b2a11bf3e0551710f252687673bcfde55ba64ac8
Tarquin <tgrovers@labviva.com>
2026-01-12 14:56:20 +0200

    Update db.py

--- db.py
+++ db.py
@@ -5,8 +5,7 @@ DATABASE_URL = "sqlite:///./messenger.db"
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False},
-)
+    connect_args={"check_same_thread": False, "timeout": 30}
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
